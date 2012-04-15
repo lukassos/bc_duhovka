bb14af93cb7ba916facdc2d5020f1cbe4601ed6d
Lukas Puchon <lukassos@gmail.com>
2012-04-15 15:37:54 +0200

    fast localization done init points done show first points to circle/polygone done

--- energyinternaltemplate.cpp
+++ energyinternaltemplate.cpp
@@ -14,27 +14,48 @@ void EnergyInternalTemplate::setCirclePositions(QList <SnakePoint> points, float
     }
 }
 
-void EnergyInternalTemplate::fastCenterLocalizationAlgorithm(CvMat image, cv::Point fastCenter, float radius){
+void EnergyInternalTemplate::fastCenterLocalizationAlgorithm(Mat image, cv::Point fastCenter, float radius){
+    //pre
+    threshold(image, image, 60, 0, THRESH_TOZERO);
+    threshold(image, image, 190, 0, THRESH_TOZERO_INV);
+    medianBlur(image, image, 3);
+    image+=150;
+    threshold(image, image, 160, 0, THRESH_TOZERO);
 
+
+
+    //rectangle of boundaries for circle found in the middle of image
     float borders[4] = {0,0,0,0}; //top,bottom,left,right
-    QSystemLocale time;
 
-    qsrand(131*QSystemLocale.TimeFormatShort);
-    float xMax = image.cols*0.3;
-    float yMax = image.rows*0.3;
-    float xOffset = image.cols*0.35;
-    float yOffset = image.rows*0.35;
+    //seed of random number depends on system time
+    QSystemLocale time;
+    qsrand(131*time.query(time.TimeFormatShort));
+
+    //30% * 30% rectangle in the middle of image
+    float xMax = image.cols*0.3; borders[3] = xOffset+(xMax/2);
+    float yMax = image.rows*0.3; borders[1] = yOffset+(yMax/2);
+    float xOffset = image.cols*0.35;  borders[2] = xOffset+(xMax/2);
+    float yOffset = image.rows*0.35;  borders[0] = yOffset+(yMax/2);
 
     for(int i=0; i<maxSize; i++){
         int randomN = qrand();
         //intensity of random pixel in 30% * 30% rectangle in the middle of image
         //(image.data.ptr + ((yOffset + randomN)%yMax)*image.step + ((xOffset+randomN) % xMax))
-        if((image.data.ptr + ((yOffset + randomN)%yMax)*image.step + ((xOffset+randomN) % xMax)) == 0){
-            if(borders[0]>
-
-
+        float actX = (yOffset + randomN) % yMax;
+        float actY = (xOffset + randomN) % xMax;
+        if((image.data.ptr + actY*image.step + actX) == 0){
+            if(borders[0] > actY)
+                borders[0] = actY;
+            if(borders[1] < actY)
+                borders[1] = actY;
+            if(borders[2] > actX)
+                borders[2] = actX;
+            if(borders[3] < actX)
+                borders[3] = actX;
         }
+
     }
-
-
+    fastCenter.x = borders[3]-borders[2];
+    fastCenter.y = borders[1]-borders[0];
+    radius = qMin((borders[3]-borders[2])/2,(borders[1]-borders[0])/2);
 }
--- snake.cpp
+++ snake.cpp
@@ -1,6 +1,6 @@
 #include "snake.h"
 
-Snake::snake()
+Snake::Snake()
 {
 
 }
@@ -8,43 +8,67 @@ Snake::snake()
 
 void Snake::initSnakeContour(Snake snake, int numberOfPoints, EnergyInternalTemplate::ContourType decision){
     snake.contour.clear();
+
+    snake.typeOfContour = decision;
     for(int i=0; i<numberOfPoints; i++){
-        SnakePoint temp = new SnakePoint();
+        SnakePoint temp;
+        temp.alpha=0,2;
+        temp.beta=0;
+        temp.step=1;
         snake.contour.append(temp);
     }
 
     switch(decision){
-        case energyInternalTemplate::ClosedContour_Circle:
-            snake.contourTemplate.;
+        case EnergyInternalTemplate::ClosedContour_Circle:
+
         break;
-        default:
+        default:;
     }
-
-
-
 }
 
 void Snake::initSnakeContour(Snake snake, int numberOfPoints, EnergyInternalTemplate::ContourType decision,
                              float offsetX, float offsetY){
-    snake.contourTemplate.type=decision;
+   // snake.contourTemplate.type=decision;
     snake.typeOfContour = decision;
 
+    snake.contour.clear();
     if(snake.contour.isEmpty() || numberOfPoints!=snake.contour.size())
         for(int i=0; i<numberOfPoints; i++){
-            SnakePoint temp = new SnakePoint();
+            SnakePoint temp;
+            temp.alpha=0,2;
+            temp.beta=0;
+            temp.step=1;
             snake.contour.append(temp);
         }
+    cv::Point fastCenter;
+    float centerX ;
+    float centerY ;
 
     switch(decision){
     case EnergyInternalTemplate::ClosedContour_Circle:
-        cv::Point fastCenter;
-        EnergyInternalTemplate::fastCenterLocalizationAlgorithm(snake.fastCenter, radius);
-        float centerX = fastCenter.x + offsetX;
-        float centerY = fastCenter.y + offsetY;
-        EnergyInternalTemplate::setCirclePositions(snake.contour, centerX, centerY, radius);
+
+        float radius;
+        //snake.contourTemplate.type = EnergyInternalTemplate::ClosedContour_Circle;
+        snake.contourTemplate.fastCenterLocalizationAlgorithm(snake.getImageOriginal(), fastCenter, radius);
+        centerX = fastCenter.x + offsetX;
+        centerY = fastCenter.y + offsetY;
+        snake.contourTemplate.setCirclePositions(snake.contour, centerX, centerY, radius);
+        snake.showMatrix(snake.getImageOriginal(), snake, "Snake After Init");
         break;
-    default:
+    default:;
     }
+}
 
+void Snake::showMatrix(Mat image, Snake snake, QString windowName){
 
+    Mat imageToShow = image.clone();
+    for(int i=0; i<snake.contour.size(); i++){
+//        const float* ptr = (const float*)(image.data.ptr + snake.contour.at(i).y*image.step + snake.contour.at(i).y);
+//        ptr* =
+        imageToShow.at<float>(snake.contour.at(i).x, snake.contour.at(i).y) = 255 - imageToShow.at<float>(snake.contour.at(i).x, snake.contour.at(i).y);
+    }
+    namedWindow( windowName.toStdString().c_str(), CV_WINDOW_AUTOSIZE );
+    imshow( windowName.toStdString().c_str(), imageToShow );
+    waitKey(0);
+    destroyWindow( windowName.toStdString().c_str() );
 }
